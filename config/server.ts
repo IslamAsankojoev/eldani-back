f02f0a3ca2bc1d05032eb6ce246da72a932f6202
Osmonkulova <Osmonkulova.Cholpon@gmail.com>
2024-02-01 00:31:49 +0600

    added new users

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,11 @@
 export default ({ env }) => ({
-	host: env("HOST", "0.0.0.0"),
-	port: env.int("PORT", 1337),
-	app: {
-		keys: env.array("APP_KEYS"),
-	},
-	webhooks: {
-		populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-	},
-	url: "https://c2ef-77-235-23-216.ngrok-free.app",
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  url: "https://2e8b-92-62-75-86.ngrok-free.app",
 });
